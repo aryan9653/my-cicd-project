c58d7c47199918593e9a615434391895ee6b4060
unknown <aryanshukla9004@gmail.com>
2026-01-09 22:49:10 +0530

    FIXED SYNTAX

--- testmain.py
+++ testmain.py
@@ -1,5 +1,4 @@
-from main import generate_html
+from main import generate_html  # Removed the ".py"
 
 def test_output():
-    # This test ensures our HTML generator works correctly
     assert "CI/CD is working!" in generate_html("User")
